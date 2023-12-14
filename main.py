f62f3abcf9423a17cd51a7a2629862fc47c5d561
Jonathan <jonathan@boeckel.name>
2023-12-15 00:12:56 +0100

    Max Velocity adjustments

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from matplotlib.animation import FuncAnimation, PillowWriter
 
 AXISMIN = -10
 AXISMAX = 10
-DOTS = 100
-ARROWSIZE = 25
+DOTS = AXISMAX * 10
 FPS = 25
 ITERATIONS = 100
+VMAXFACTOR = 0.1
 
 
 # takes arrays of coordinates of particles and the velocities
@@ -25,12 +25,6 @@ def renderPlot(pos, vel, func):
     yf = np.linspace(AXISMIN, AXISMAX, DOTS)
     Xf, Yf = np.meshgrid(xf, yf)
 
-    # arrange returns array of evenly spaced values (this could be replaced by arrays of positions from the pso algorithm)
-    x1 = np.arange(0, -2, -0.02)
-    y1 = x1 ** 2
-    #x1 = np.arange(0, -0.2, -0.002)
-    #y1 = np.arange(0, -0.2, -0.002)
-
     # gets called with i as the frame number
     def animate(i):
         ax.clear()  # clear the diagram
@@ -39,11 +33,13 @@ def renderPlot(pos, vel, func):
         ax.set_aspect('equal')
         ax.axis([AXISMIN, AXISMAX, AXISMIN, AXISMAX])
 
-        #line, = ax.plot(x1[0:i], y1[0:i], color='red', lw=1)  # draws the line the point is traveling
-        #point1, = ax.plot(x1[i], y1[i], marker='X', color='red')  # draws the moving point
+        # line, = ax.plot(x1[0:i], y1[0:i], color='red', lw=1)  # draws the line the point is traveling
+        # point1, = ax.plot(x1[i], y1[i], marker='X', color='red')  # draws the moving point
 
         points = []
         for j in range(0, len(pos[0])):
+            # xs = np.arange(pos[i][j][0])
+            # for k in range()
             p = ax.plot(pos[i][j][0], pos[i][j][1], marker='X', color='red')  # draws the moving point
             points.append(p)
 
@@ -53,47 +49,77 @@ def renderPlot(pos, vel, func):
     ani.save("TLI.gif", dpi=300, writer=PillowWriter(fps=FPS))
     print("Animation saved")
 
-def standardParticleSwarmOptimization(swarmSize, func, c1 = 0.1, c2 = 0.1, w = 0.1):
-    particles = [] # in the first element is the positional data of all the particles in the first iteration
+
+def getLengthOfVector(ar):
+    return np.sqrt(ar[0] ** 2 + ar[1] ** 2)
+
+
+def getClampedVel(newVel, delta=VMAXFACTOR):
+    vMaxValue = delta * (np.abs(AXISMAX) + np.abs(AXISMIN))  # max allowed velocity
+    newVelValue = getLengthOfVector(newVel)  # value / betrag of new calculated velocity
+
+    if newVelValue >= vMaxValue:
+        normalisedNewVel = [newVel[0] / newVelValue,
+                            newVel[1] / newVelValue]  # normalised vector of new calculated velocity
+        return [vMaxValue * normalisedNewVel[0], vMaxValue * normalisedNewVel[1]]
+    else:
+        return newVel
+
+
+def standardParticleSwarmOptimization(func, c1=0.1, c2=0.1, w=0.1):
+    particles = []  # in the first element is the positional data of all the particles in the first iteration
     velocities = []
     personalBests = []
-    globalBest = []
 
     # append arrays for first iteration
     particles.append([])
     velocities.append([])
 
     # initialize positions
-    for i in range(swarmSize):
-        particles[0].append([0, i])
-        velocities[0].append([0, 0])
-        personalBests.append(particles[0][i])
+    step = (np.abs(AXISMIN) + np.abs(AXISMAX)) / 8
+    x, y = np.arange(AXISMIN, AXISMAX, step), np.arange(AXISMIN, AXISMAX, step)
+    partCount = 0
+    for a in x:
+        for b in y:
+            particles[0].append([a, b])
+            velocities[0].append([0, 0])
+            personalBests.append(particles[0][partCount])
+            partCount = partCount + 1
 
     # initialize the global best to the value of the first particle
     globalBest = particles[0][0]
 
     # update the global best to the best position of a particle
-    for i in range(swarmSize):
-        if func(particles[0][i][0], particles[0][i][1]) < func(globalBest[0], globalBest[1]):
+    for i in range(partCount):
+        if np.all(func(particles[0][i][0], particles[0][i][1]) < func(globalBest[0], globalBest[1])):
             globalBest = particles[0][i]
 
     t = 1
     while t < ITERATIONS:
         particles.append([])
         velocities.append([])
-        for i in range(swarmSize): #foreach particle
-            #compute new velocities
+        for i in range(partCount):  # foreach particle
+            lastPos = particles[t - 1][i]
+
+            # compute new velocities
             r1 = random.random()
             r2 = random.random()
-            origVel = [w * velocities[t-1][i][0], w * velocities[t-1][i][1]]
-            cogComp = [c1*r1*(personalBests[i][0]-particles[t-1][i][0]), c1*r1*(personalBests[i][1]-particles[t-1][i][1])]
-            socComp = [c2*r2*(globalBest[0] - particles[t-1][i][0]), c2*r2*(globalBest[1] - particles[t-1][i][1])]
-            velocities[t].append([origVel[0] + cogComp[0] + socComp[0], origVel[1] + cogComp[1] + socComp[1]])
+            origVel = [w * velocities[t - 1][i][0],
+                       w * velocities[t - 1][i][1]]
+            cogComp = [c1 * r1 * (personalBests[i][0] - lastPos[0]),
+                       c1 * r1 * (personalBests[i][1] - lastPos[1])]
+            socComp = [c2 * r2 * (globalBest[0] - lastPos[0]),
+                       c2 * r2 * (globalBest[1] - lastPos[1])]
+            newVel = [origVel[0] + cogComp[0] + socComp[0],
+                      origVel[1] + cogComp[1] + socComp[1]]
 
-            #compute new positions
-            particles[t].append([particles[t-1][i][1] + velocities[t][i][0], particles[t-1][i][1] + velocities[t][i][1]])
+            velocities[t].append(getClampedVel(newVel))
 
-            #update personal and global bests
+            # compute new positions
+            particles[t].append(
+                [particles[t - 1][i][1] + velocities[t][i][0], particles[t - 1][i][1] + velocities[t][i][1]])
+
+            # update personal and global bests
             if func(particles[t][i][0], particles[t][i][1]) < func(personalBests[i][0], personalBests[i][1]):
                 personalBests[i] = particles[t][i]
             if func(particles[t][i][0], particles[t][i][1]) < func(globalBest[0], globalBest[1]):
@@ -102,8 +128,6 @@ def standardParticleSwarmOptimization(swarmSize, func, c1 = 0.1, c2 = 0.1, w = 0
         t = t + 1
 
     return particles, velocities
-
-    #TODO velocity clamping is needed asap!
 
 
 def rosenbrock_function(x, y):
@@ -128,6 +152,24 @@ def quadratic_function(x, y):
     return a * x ** 2 + b * y ** 2 + c * x * y + d * x + e * y + f
 
 
+def ackley_function(x, y):
+    c_x = 1.5  # Coefficient for the x term
+    c_y = 0.8  # Coefficient for the y term
+    term1 = -20 * np.exp(-0.2 * np.sqrt(0.5 * (c_x * x**2 + c_y * y**2)))
+    term2 = -np.exp(0.5 * (np.cos(2 * np.pi * c_x * x) + np.cos(2 * np.pi * c_y * y)))
+    term3 = np.exp(1) + 20
+    return term1 + term2 + term3
+
+
+def himmelblau_function(x, y):
+    term1 = (x**2 + y - 11)**2
+    term2 = (x + y**2 - 7)**2
+    return term1 + term2
+
+def other_function(x,y):
+    return np.sin(x) + np.cos(y)
+
+
 if __name__ == '__main__':
     # positions of particles
     X = [1, 2, 3, 4]
@@ -137,6 +179,12 @@ if __name__ == '__main__':
     xVel = [1, 2, 3, -2]
     yVel = [2, 5, -3, 0]
 
-    pos, vel = standardParticleSwarmOptimization(5, quadratic_function, 5)
+    # selectedFunc = rastrigin_function
+    # selectedFunc = rosenbrock_function
+    # selectedFunc = quadratic_function
+    # selectedFunc = ackley_function
+    # selectedFunc = himmelblau_function
+    selectedFunc = other_function
+    pos, vel = standardParticleSwarmOptimization(selectedFunc)
     print("done computing, starting rendering now")
-    renderPlot(pos, vel, quadratic_function)
+    renderPlot(pos, vel, selectedFunc)
